ed7e65af5df8eb20573377504c2d5da34f7d45af
juareix <juarezmaciel45@gmail.com>
2022-11-25 16:34:28 -0400

    aditioning functions

--- script.js
+++ script.js
@@ -10,3 +10,67 @@ let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 
 let numeros = document.querySelector('.d-1-3');
+
+//variaveis de ambiente
+let etapaAtual = 0; //para saber em qual etapa eu me encontro do processo
+
+let numero = ''; //pra saber o numer    q    ta sendo preendchifp
+
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual]; //ta puxando os dados da string etapas no outro arquivo js
+    
+    let numeroHtml = '';
+
+    for(let i=0; i<etapa.numeros; i++){
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>';
+        }
+        else{
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none'
+
+    cargo.innerHTML = etapa.titulo;
+
+    descricao.innerHTML = '';
+
+    aviso.style.display = 'none';
+
+    lateral.innerHTML = '';
+
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface(){
+
+}
+
+function clicou(n){
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null){
+        elNumero.innerHTML = n; 
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca');
+        }
+        else{
+            atualizaInterface()
+        }
+    }
+}
+function branco() {
+    
+}
+function corrige(){
+
+}
+function confirma(){
+
+}
+
+comecarEtapa();
